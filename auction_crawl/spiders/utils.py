c36ea57a2542fc53f7124d4d374d481034e980ab
Liza Leung <liza.leung@quokkareward.com>
2021-07-06 11:33:25 +0800

    initial commit

---
+++ auction_crawl/spiders/utils.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import datetime
+import json
+
+today = datetime.datetime.today().strftime('%Y%m%d')
+
+outdir = os.environ['OUTDIR_BASE']
+# outdir = "."
+outdir = "%s/%s/" %(outdir,today)
+
+
+def writeToFile(spider_name, filename, content, mode = 'w' ):
+    full_path = os.path.join(outdir, spider_name, filename)
+    base_path = os.path.dirname(full_path)
+
+    if not os.path.exists(base_path): os.makedirs(base_path)
+
+    if mode == "wb":
+        with open(full_path, mode) as f:
+            f.write(content)
+    else:
+        with open(full_path, mode, encoding='utf8') as f:
+            f.write(content)
+    return
